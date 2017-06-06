da56b53538c261c927979593b2f1c26fa273c607
kpam92 <kpam1992@gmail.com>
2017-06-06 14:37:48 -0700

    navbar styled

--- frontend/components/nav/nav.jsx
+++ frontend/components/nav/nav.jsx
@@ -12,11 +12,25 @@ class Nav extends React.Component {
     return(
       <div className="navbar">
         <ul>
-           <li><a className="active" href="#home">Home</a></li>
-           <li><a href="#news">News</a></li>
-           <li><a href="#contact">Contact</a></li>
-           <li><a href="#about">About</a></li>
-           <li><img src="http://res.cloudinary.com/dt5viyxyq/image/upload/c_scale,w_91/v1496782016/plus_icon_m3ccte.png"/></li>
+          <div className='left'>
+             <li>
+               <a href="#">
+                 <img src="http://res.cloudinary.com/dt5viyxyq/image/upload/v1496784960/Screen_Shot_2017-06-06_at_2.35.31_PM_iexbim.png"/>
+               </a>
+             </li>
+          </div>
+           <div className='right'>
+             <li>
+               <a href="#">
+                 <img src="http://res.cloudinary.com/dt5viyxyq/image/upload/c_scale,w_91/v1496782016/plus_icon_m3ccte.png"/>
+               </a>
+             </li>
+             <li>
+               <a href="#">
+                 <img className='prof' src="http://res.cloudinary.com/dt5viyxyq/image/upload/v1475112850/1536648_10153300643313893_2943769392509528369_n_ymqsol.jpg"/>
+               </a>
+             </li>
+           </div>
          </ul>
      </div>
     )
