1e430ce9186bc536605db1892bf55db985714c21
kpam92 <kpam1992@gmail.com>
2017-06-06 14:10:16 -0700

    styling navar

--- frontend/components/nav/nav.jsx
+++ frontend/components/nav/nav.jsx
@@ -10,8 +10,14 @@ class Nav extends React.Component {
 
   render() {
     return(
-      <div>
-       <h1>this is the navbar</h1>
+      <div className="navbar">
+        <ul>
+           <li><a className="active" href="#home">Home</a></li>
+           <li><a href="#news">News</a></li>
+           <li><a href="#contact">Contact</a></li>
+           <li><a href="#about">About</a></li>
+           <li><img src="http://res.cloudinary.com/dt5viyxyq/image/upload/c_scale,w_91/v1496782016/plus_icon_m3ccte.png"/></li>
+         </ul>
      </div>
     )
   }
