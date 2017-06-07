5513d5ff680db3983126b9f6580a7349732fe42f
kpam92 <kpam1992@gmail.com>
2017-06-07 13:22:32 -0700

    heroku deploy

--- frontend/components/profile/profile.jsx
+++ frontend/components/profile/profile.jsx
@@ -1,25 +1,39 @@
 import React from 'react';
 import PhotoIndexItem from '../feed/photo_index';
+
+import { fetchUser } from '../../util/user_api_util';
+
 class Feed extends React.Component {
   constructor(props){
     super(props)
   }
 
-  componentDidMount() {
+  componentWillMount() {
+    // console.log(lala)
     this.props.fetchUser(this.props.match.params.userId)
+    // const user = fetchUser(this.props.match.params.userId).then(suc => suc.photos.map(photo => (
+    //     return photo.image_url
+    //   )
+    // ))
+    // console.log(user)
+
   }
 
   render() {
+    // const user = fetchUser(this.props.match.params.userId).then(suc => suc.photos.map(photo => (
+    //     <PhotoIndexItem key={photo.id} photo={photo}/>
+    //   )
+    // ))
     // const { photos } = this.props.user;
-    // const photoDetails = this.props.userphotos.map(photo => (
+    // const photoDetails = user.photos.map(photo => (
     //     <PhotoIndexItem key={photo.id} photo={photo}/>
     //   )
     // );
-
     return(
       <div className='feed'>
        <ul>
          this is the profile talking
+         {}
        </ul>
      </div>
     )
