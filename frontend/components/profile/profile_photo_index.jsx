8f731519827e3224a44f756956b519db5df310ea
kpam92 <kpam1992@gmail.com>
2017-06-07 19:12:58 -0700

    initial set up of profile photo index

--- frontend/components/profile/profile_photo_index.jsx
+++ frontend/components/profile/profile_photo_index.jsx
@@ -4,10 +4,35 @@ class ProfilePhotoIndex extends React.Component {
   constructor(props){
     super(props)
   }
+  renderPhotos() {
+    const { photos } = this.props;
+    if (photos.length < 1) {
+      return;
+    }
+
+    const content = photos.map((photo) => (
+      <li key={`photo-${photo.id}`}>
+        <button
+          className="image-button"
+        >
+          <img
+            src={photo.image_url}
+            alt={`photo-${photo.id}`}
+          />
+        </button>
+      </li>
+    ));
+
+    return content;
+  }
 
   render(){
     return(
-
+      <div className="photos">
+        <ul>
+          {this.renderPhotos()}
+        </ul>
+      </div>
     )
   }
 }
