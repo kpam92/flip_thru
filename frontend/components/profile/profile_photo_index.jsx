991fe50e97fbc0dd86ffdefe16204ea3b0f493ef
kpam92 <kpam1992@gmail.com>
2017-06-07 18:51:30 -0700

    creating profile photo index component

---
+++ frontend/components/profile/profile_photo_index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class ProfilePhotoIndex extends React.Component {
+  constructor(props){
+    super(props)
+  }
+
+  render(){
+    return(
+
+    )
+  }
+}
+
+export default ProfilePhotoIndex;
